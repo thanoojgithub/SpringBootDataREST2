241d340284519323d1d8967bf8a98100ef360945
thanooj kalathuru <javaeecub@gmail.com>
2024-12-15 16:00:21 +0530

    Update SpringBootDataRest2Application.java

--- src/main/java/com/springboot/SpringBootDataRest2Application.java
+++ src/main/java/com/springboot/SpringBootDataRest2Application.java
@@ -1,7 +1,13 @@
 package com.springboot;
 
+import jakarta.persistence.*;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class SpringBootDataRest2Application {
@@ -9,5 +15,54 @@ public class SpringBootDataRest2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDataRest2Application.class, args);
 	}
+}
 
+@Entity
+@Table(name = "customers")
+class Customer {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String email;
+
+	// Getters and Setters
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
+
+@RepositoryRestResource
+interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
+
+@Component
+class CustomHealthIndicator implements HealthIndicator {
+
+	@Override
+	public Health health() {
+		return Health.up().withDetail("Custom Health Check", "Application is running").build();
+	}
 }
